5638d70b1bfeac08d71af33bc20f3a05c7c160f9
hugosenari <hugosenari@gmail.com>
2022-03-12 02:50:37 -0300

    chore: re index gamelight

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A set of simple modules for writing a JavaScript 2D game.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."gamelight-master".url = "path:./master";
+  inputs."gamelight-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."gamelight-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,58 @@
+{
+  description = ''A set of simple modules for writing a JavaScript 2D game.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-gamelight-master.flake = false;
+  inputs.src-gamelight-master.owner = "dom96";
+  inputs.src-gamelight-master.ref   = "refs/heads/master";
+  inputs.src-gamelight-master.repo  = "gamelight";
+  inputs.src-gamelight-master.type  = "github";
+  
+  inputs."sdl2".dir   = "nimpkgs/s/sdl2";
+  inputs."sdl2".owner = "riinr";
+  inputs."sdl2".ref   = "flake-pinning";
+  inputs."sdl2".repo  = "flake-nimble";
+  inputs."sdl2".type  = "github";
+  inputs."sdl2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."sdl2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."chroma".dir   = "nimpkgs/c/chroma";
+  inputs."chroma".owner = "riinr";
+  inputs."chroma".ref   = "flake-pinning";
+  inputs."chroma".repo  = "flake-nimble";
+  inputs."chroma".type  = "github";
+  inputs."chroma".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."chroma".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."typography".dir   = "nimpkgs/t/typography";
+  inputs."typography".owner = "riinr";
+  inputs."typography".ref   = "flake-pinning";
+  inputs."typography".repo  = "flake-nimble";
+  inputs."typography".type  = "github";
+  inputs."typography".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."typography".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."flippy".dir   = "nimpkgs/f/flippy";
+  inputs."flippy".owner = "riinr";
+  inputs."flippy".ref   = "flake-pinning";
+  inputs."flippy".repo  = "flake-nimble";
+  inputs."flippy".type  = "github";
+  inputs."flippy".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."flippy".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-gamelight-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-gamelight-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
